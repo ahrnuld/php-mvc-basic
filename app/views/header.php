05b9097b77f54a9c6afea7b68b5540c0d14c99c2
Max Kruiswegt <112982786+ItsMaxDev@users.noreply.github.com>
2023-12-05 14:13:38 +0100

    Fix About Page linking

--- app/views/header.php
+++ app/views/header.php
@@ -10,7 +10,7 @@
     <header class="d-flex justify-content-center py-3">
       <ul class="nav nav-pills">
       <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
-        <li class="nav-item"><a href="/about" class="nav-link">About</a></li>
+        <li class="nav-item"><a href="/home/about" class="nav-link">About</a></li>
         <li class="nav-item"><a href="/article" class="nav-link">Manage articles</a></li>
       </ul>
     </header>
